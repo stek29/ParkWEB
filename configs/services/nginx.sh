d7d0b37bcc10156e06c853db3287b59def85845b
Viktor Oreshkin <imselfish@stek29.rocks>
2017-12-02 14:32:42 +0300

    Better nginx conf

--- configs/services/nginx.sh
+++ configs/services/nginx.sh
@@ -4,6 +4,15 @@ NGINX_DIR=/tmp/tp-askmeme
 PID_FILE="$NGINX_DIR/logs/nginx.pid"
 CONF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
+link_mimes() {
+  orig_conf_dir="$(nginx -t 2>&1 |\
+    egrep -o '/(.*)nginx\.conf' |\
+    head -n1 |\
+    sed 's/nginx.conf//')"
+  rm -f "$CONF_DIR/mime.types"
+  ln -s "$orig_conf_dir/mime.types" "$CONF_DIR/mime.types"
+}
+
 start_nginx() {
   if [ ! -e "$PID_FILE" ]; then
     mkdir -p "$NGINX_DIR/logs"
@@ -18,6 +27,10 @@ stop_nginx() {
   fi
 }
 
+if [ ! -e "$CONF_DIR/mime.types" ]; then
+  link_mimes
+fi
+
 case "$1" in
   start)
     start_nginx
@@ -29,6 +42,9 @@ case "$1" in
     stop_nginx
     start_nginx
     ;;
+  linkmimes)
+    link_memes
+    ;;
   *)
-    echo "Usage: $0 {start|stop|restart}"
+    echo "Usage: $0 {start|stop|restart|linkmimes}"
 esac
